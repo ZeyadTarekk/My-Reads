8ac932351d749cbdfecd8d9a3b78083999155137
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-07-14 18:08:02 +0200

    make the search different component

---
+++ src/components/SearchPage.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+const SearchPage = () => {
+  const [showSearchPage, setShowSearchpage] = useState(false);
+
+  return (
+    <div className="search-books">
+      <div className="search-books-bar">
+        <a
+          className="close-search"
+          onClick={() => setShowSearchpage(!showSearchPage)}
+        >
+          Close
+        </a>
+        <div className="search-books-input-wrapper">
+          <input type="text" placeholder="Search by title, author, or ISBN" />
+        </div>
+      </div>
+      <div className="search-books-results">
+        <ol className="books-grid"></ol>
+      </div>
+    </div>
+  );
+};
+
+export default SearchPage;
